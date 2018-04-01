424a449c97a1e6425a017953699c05c8fbf7edb7
evan <test@test>
2018-03-31 18:36:14 -0700

    This is a only a test.

--- desktop/src/com/mygdx/demonestate/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/demonestate/desktop/DesktopLauncher.java
@@ -5,14 +5,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.demonestate.DemonEstate;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1820;
 		config.height = 980;
 		config.title = "Demon Estate";
-		config.vSyncEnabled = false; // Setting to false disables vertical sync
+//		config.vSyncEnabled = false; // Setting to false disables vertical sync
 		config.foregroundFPS = 0; // Setting to 0 disables foreground fps throttling
 		config.backgroundFPS = 0;
 		new LwjglApplication(new DemonEstate(), config);
 	}
 }
+
+// remote skeleton branch test
