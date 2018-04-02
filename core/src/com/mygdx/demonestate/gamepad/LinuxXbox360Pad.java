d3d85210489e1aa23fdc88ab6dd7f44837959790
david <daggad95@gmail.com>
2018-04-02 13:16:43 -0700

    refactored gamepad configuration to be easier to change

--- core/src/com/mygdx/demonestate/gamepad/LinuxXbox360Pad.java
+++ core/src/com/mygdx/demonestate/gamepad/LinuxXbox360Pad.java
@@ -1,11 +1,11 @@
-package com.mygdx.demonestate;
+package com.mygdx.demonestate.gamepad;
 
 import com.badlogic.gdx.controllers.PovDirection;
 
 /**
  * Created by david on 3/21/17.
  */
-public class LinuxXbox360Pad {
+public class LinuxXbox360Pad implements Xbox360Pad {
     public static final int BUTTON_X = 2;
     public static final int BUTTON_Y = 3;
     public static final int BUTTON_A = 0;
@@ -26,4 +26,25 @@ public class LinuxXbox360Pad {
     public static final int AXIS_RIGHT_X = 3; //-1 is left | +1 is right
     public static final int AXIS_RIGHT_Y = 4; //-1 is up | +1 is down
     public static final int AXIS_RIGHT_TRIGGER = 5; //value 0 to -1f
+
+    public int buttonX() { return BUTTON_X; }
+    public int buttonY() { return BUTTON_Y; }
+    public int buttonA() { return BUTTON_A; }
+    public int buttonB() { return BUTTON_B; }
+    public int buttonBack() { return BUTTON_BACK; }
+    public int buttonStart() { return BUTTON_START; }
+    public PovDirection buttonDpadUp() { return BUTTON_DPAD_UP; }
+    public PovDirection buttonDpadDown() { return BUTTON_DPAD_DOWN; };
+    public PovDirection buttonDpadRight() { return BUTTON_DPAD_RIGHT; };
+    public PovDirection buttonDpadLeft() { return BUTTON_DPAD_RIGHT; };
+    public int buttonLB() { return BUTTON_LB; };
+    public int buttonRB() { return BUTTON_RB; }
+    public int buttonL3() { return BUTTON_L3; }
+    public int buttonR3() { return BUTTON_R3; }
+    public int axisLeftX() { return AXIS_LEFT_X; }
+    public int axisLeftY() { return AXIS_LEFT_Y; };
+    public int axisLeftTrigger() { return AXIS_LEFT_TRIGGER; }
+    public int axisRightX() { return AXIS_RIGHT_X; }
+    public int axisRightY() { return AXIS_RIGHT_Y; }
+    public int axisRightTrigger() { return AXIS_RIGHT_TRIGGER; }
 }
